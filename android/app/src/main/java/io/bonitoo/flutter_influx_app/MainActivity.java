e5186618b7efc67a3e1836cf3549b2841fb17233
Robert Hajek <robert.hajek@gmail.com>
2022-02-09 14:47:07 +0100

    feat: add macos, linux, windows and web profiles, add docker for testing

--- android/app/src/main/java/io/bonitoo/flutter_influx_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package io.bonitoo.flutter_influx_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/io/iot_center_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.iot_center_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

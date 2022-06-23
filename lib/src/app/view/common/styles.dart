3d6279e7f25b26a54422a955404d47f8ca48e9c3
michaelahojna <michaela.hojna@gmail.com>
2022-06-23 13:56:00 +0200

    refactor: refactoring & readme

--- lib/src/app/view/common/styles.dart
+++ lib/src/app/view/common/styles.dart
@@ -1,8 +1,10 @@
 import 'package:iot_center_flutter_mvc/src/view.dart';
 
 const Color darkBlue = Color.fromRGBO(2, 10, 71, 1);
+const Color turquoise = Color.fromRGBO(94, 228, 228, 1);
 const Color pink = Color.fromRGBO(211, 9, 113, 1);
 const Color purple = Color.fromRGBO(155, 42, 255, 1);
+const Color blue = Color.fromRGBO(147, 148, 255, 1);
 const Color lightGrey = Color.fromRGBO(250, 250, 250, 1);
 const Color darkGrey = Color.fromRGBO(86, 86, 86, 1.0);
 
